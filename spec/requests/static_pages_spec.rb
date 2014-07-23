22f4e2b35e353d9a9b840a9e1d2cc9aefe566428
dineshbabur92 <dineshbabur92@gmail.com>
2014-07-23 15:42:07 +0530

    Add contact page and use let to avoid duplicates in test ruby file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) {"Ruby on Rails Tutorial"}
 	describe "Home page" do
 	
 		it "should have the content 'Sample App'" do
@@ -21,7 +22,7 @@ describe "Static pages" do
 		
 		it "should have the title 'Ruby on Rails Tutorial | Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title('Ruby on Rails Tutorial | Home')
+			expect(page).to have_title("#{base_title} | Home")
 		end
 		
 	end
@@ -35,7 +36,7 @@ describe "Static pages" do
 		
 		it "should have the title 'Ruby on Rails Tutorial | Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title('Ruby on Rails Tutorial | Help')
+			expect(page).to have_title("#{base_title} | Help")
 		end
 		
 	end
@@ -49,9 +50,24 @@ describe "Static pages" do
 		
 		it "should have the title 'Ruby on Rails Tutorial | About Us'" do
 			visit '/static_pages/about'
-			expect(page).to have_title('Ruby on Rails Tutorial | About Us')
+			expect(page).to have_title("#{base_title} | About Us")
 		end
 		
 	end
+	
+	describe "Contact page" do
+	
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+		
+		it "should have the title 'Ruby on Rails Tutorial | Contact Us'" do
+		visit '/static_pages/contact'
+		expect(page).to have_title("#{base_title} | Contact Us")
+		end
+	
+	end
+	
 end
 
