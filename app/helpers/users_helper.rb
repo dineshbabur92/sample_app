56c41b8207130d0787aa0957ce4cce348d15e6d6
dineshbabur92 <dineshbabur92@gmail.com>
2014-11-27 17:27:37 +0530

    finish section 8.2.1 to add remember token to user model

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
 
 	def gravatar_for(user)
 		gravatar_id=Digest::MD5::hexdigest(user.email.downcase)
-		gravater_url="https://secure.gravatar.com/avatars/#{ gravatar_id }.png"
-		image_tag(gravater_url, alt: user.name, class: "gravatar" )
+		gravatar_url="https://secure.gravatar.com/avatars/#{ gravatar_id }.png"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar" )
 	end
 end
 
